066ed75de6be5cce4e02b3cb8aa9f800359dba34
Thomas Cocchiara <thomas.cocchiara@gmail.com>
2025-09-21 18:22:08 +0200

    Add admin user impersonation support

--- app/Helpers/user_helper.php
+++ app/Helpers/user_helper.php
@@ -4,18 +4,47 @@ function jwt_decode($token) {
     return json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))), true);
 }
 
+function get_jwt_payload(): array {
+    $header = $_SERVER['HTTP_AUTHORIZATION'] ?? null;
+
+    if (!$header) {
+        return [];
+    }
+
+    $parts = explode(' ', $header);
+
+    if (count($parts) < 2 || strtolower($parts[0]) !== 'bearer') {
+        return [];
+    }
+
+    $token = $parts[1];
+
+    if (!$token) {
+        return [];
+    }
+
+    return jwt_decode($token) ?? [];
+}
+
 function get_user_id() {
-    $header = $_SERVER['HTTP_AUTHORIZATION'];
+    $user_info = get_jwt_payload();
 
-    $token = explode(" ", $header)[1];
+    return $user_info['sub'] ?? null;
+}
 
-    $user_info = jwt_decode($token);
+function get_impersonator_id() {
+    $payload = get_jwt_payload();
 
-    return $user_info['sub'];
+    return $payload['impersonator_id'] ?? null;
 }
 
 function get_user_by_auth_header() {
     $user_id = get_user_id();
+
+    if (!$user_id) {
+        return null;
+    }
+
     $user = \App\Models\User::firstWhere('id', $user_id);
 
     return $user;
